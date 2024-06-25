4b74cd203f2e2f6983d02b7ba8d33a6c446b2acc
EC2 Default User <ec2-user@ip-172-31-40-147.ec2.internal>
2024-06-25 22:16:22 +0000

    Added state backup and fixed variables.

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "aws_region" {
   description = "AWS region where resources will be created."
   default     = "us-east-1"
 }
-
-variable "tfstate_bucket" {
-  description = "The S3 bucket that will save remotely our tfstate's backup."
-  default     = "my-tfstate-bucket"
-}
